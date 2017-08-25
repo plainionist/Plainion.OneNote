5fa43acfb3ed883c922cdeeb5388d279e6c9d040
plainionist <pinguin751@gmx.net>
2017-08-25 13:50:56 +0200

    readme improved

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 
-NoteBook app based on 
+# Plainion.OneNote: ONE notebook to rule them all 
+
+Simple WPF application based on 
 [Plainion.Windows.Controls.Text.NoteBook](https://github.com/plainionist/Plainion.Windows/tree/master/src/Plainion.Windows/Controls/Text).
+
+![](docs/Overview.png)
 
--- src/Plainion.OneNote/ViewModels/NoteBookViewModel.cs
+++ src/Plainion.OneNote/ViewModels/NoteBookViewModel.cs
@@ -1,9 +1,42 @@
 ﻿using System.ComponentModel.Composition;
+using System.Windows.Documents;
+using Plainion.IO.MemoryFS;
+using Plainion.Windows.Controls.Text;
+using Plainion.Windows.Mvvm;
 
 namespace Plainion.OneNote.ViewModels
 {
     [Export]
-    class NoteBookViewModel
+    class NoteBookViewModel : BindableBase
     {
+        private FileSystemDocumentStore myDocumentStore;
+
+        public NoteBookViewModel()
+        {
+            var fs = new FileSystemImpl();
+
+            DocumentStore = new FileSystemDocumentStore(fs.Directory("/x"));
+            DocumentStore.Initialize();
+
+            DocumentStore.Create("/User documentation/Installation").Body.AddText("Installation");
+            DocumentStore.Create("/User documentation/Getting started").Body.AddText("Getting started");
+            DocumentStore.Create("/User documentation/FAQ").Body.AddText("Frequenty Asked Questions");
+            DocumentStore.Create("/Developer documentation/Getting started").Body.AddText("Getting started as a developer");
+            DocumentStore.Create("/Developer documentation/HowTos/MVC with F#").Body.AddText("MVC with F#");
+            DocumentStore.Create("/Developer documentation/HowTos/WebApi with F#").Body.AddText("WebApi with F#");
+        }
+        public FileSystemDocumentStore DocumentStore
+        {
+            get { return myDocumentStore; }
+            private set { SetProperty(ref myDocumentStore, value); }
+        }
+    }
+    static class FlowDocumentExtensions
+    {
+        public static FlowDocument AddText(this FlowDocument self, string text)
+        {
+            self.Blocks.Add(new Paragraph(new Run(text)));
+            return self;
+        }
     }
 }
