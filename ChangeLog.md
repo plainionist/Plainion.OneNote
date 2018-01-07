09935883f29dc56c2f2268629d56c7b902aad375
plainionist <pinguin751@gmx.net>
2018-01-07 18:42:07 +0100

    Plainion.Core and Plainion.Windows updated for bug fixed

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,4 +1,9 @@
 
+## 1.2 - 2018-01-07
+
+- updated Plainion.Windows
+- updated Plainion.Core
+
 ## 1.1 - 2017-12-27
 
 - updated to Plainion.Windows 2.5 
--- src/Plainion.OneNote/Properties/AssemblyInfo.cs
+++ src/Plainion.OneNote/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Reflection;
 [assembly: AssemblyProductAttribute("Plainion.OneNote")]
 [assembly: AssemblyDescriptionAttribute("Plainion.OneNote")]
 [assembly: AssemblyCopyrightAttribute("Copyright @ 2018")]
-[assembly: AssemblyVersionAttribute("1.1")]
-[assembly: AssemblyFileVersionAttribute("1.1")]
+[assembly: AssemblyVersionAttribute("1.2")]
+[assembly: AssemblyFileVersionAttribute("1.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Plainion.OneNote";
         internal const System.String AssemblyProduct = "Plainion.OneNote";
         internal const System.String AssemblyDescription = "Plainion.OneNote";
         internal const System.String AssemblyCopyright = "Copyright @ 2018";
-        internal const System.String AssemblyVersion = "1.1";
-        internal const System.String AssemblyFileVersion = "1.1";
+        internal const System.String AssemblyVersion = "1.2";
+        internal const System.String AssemblyFileVersion = "1.2";
     }
 }
